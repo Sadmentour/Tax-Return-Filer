540ed7f60acfe4c9ea6d5084da3edaf52b3f6bb8
Sadmentour <sadmentores@gmail.com>
2025-08-11 00:50:13 +0300

    Renamed brackets.json to financialConstants.json

--- FinancialConstants.py
+++ FinancialConstants.py
@@ -3,15 +3,34 @@ import json
 class FinancialConstants:
     def __init__(self):
         self.pension_bracket_length: int = 0
-        self.rent_bracket_length: int = 0
         self.pension_brackets: list[float] = []
+        self.rent_bracket_length: int = 0
         self.rent_brackets: list[float] = []
 
     def get_constants(self):
         pass
 
-    def get_tax_brackets(self, file: str = "brackets.json"):
+    def get_tax_brackets(self, file: str = "financialConstants.json"):
+        pension_brackets: list = []
+        rent_brackets: list = []
+
         with open(file, "r") as brackets:
             tax_brackets: dict = json.load(brackets)
-            self.pension_brackets = tax_brackets["pension_brackets"]
-            self.rent_brackets = tax_brackets["rent_brackets"]
+        
+        self.pension_bracket_length = tax_brackets["pension_data"]["brackets_length"]
+        self.rent_bracket_length = tax_brackets["rent_data"]["brackets_length"]
+        
+        self.pension_brackets = self.parse_brackets_from_json("pension_data", tax_brackets)
+        self.rent_brackets = self.parse_brackets_from_json("rent_data", tax_brackets)
+
+    @staticmethod
+    def parse_brackets_from_json(data_field: str, constants_object: dict):
+        brackets: list = []
+        for bracket in constants_object[data_field]["brackets"].items():
+            brackets.append(bracket)
+        return brackets
+
+
+if __name__ == "__main__":
+    c1 = FinancialConstants()
+    print(c1.get_tax_brackets())
