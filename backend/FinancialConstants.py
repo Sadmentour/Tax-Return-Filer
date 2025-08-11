1faf30cc3ff6ac7b9704a79c36376878adb639fa
Sadmentour <sadmentores@gmail.com>
2025-08-11 17:28:16 +0300

    File 'FinancialConstants.py' completed. (for now...)

--- backend/FinancialConstants.py
+++ backend/FinancialConstants.py
@@ -2,33 +2,40 @@ from utils.conversions import monthly_to_yearly
 import json
 
 class FinancialConstants:
-    def __init__(self):
+    def __init__(self, file: str = "backend/financialConstants.json"):
         self.pension_brackets: dict = {}
         self.pension_brackets_length: int = 0
+        self.pension_stamp_duty: float = 0
         self.rent_brackets: dict = {}
         self.rent_brackets_length: int = 0
-        self.pension_brackets, self.rent_brackets = self.get_tax_brackets()
-        
+        self.constants: dict = {}
+        with open(file, "r") as constants:
+            self.constants: dict = json.load(constants)
+        self.pension_brackets, self.rent_brackets = self.get_tax_brackets(self.constants)
 
-    def get_constants(self):
-        pass
+    def init_constants(self):
+        self.get_tax_brackets(self.constants)
 
-    def get_tax_brackets(self, file: str = "backend/financialConstants.json"):
+        self.pension_stamp_duty = self.get_stamp_duty("pension_data", self.constants)
+        self.rent_stamp_duty = self.get_stamp_duty("rent_data", self.constants)
+
+    def get_tax_brackets(self, constants: dict):
         pension_brackets: dict = {}
         rent_brackets: dict = {}
-        with open(file, "r") as constants:
-            tax_constants: dict = json.load(constants)
 
-        self.pension_brackets_length = tax_constants["pension_data"]["brackets_length"]
-        self.rent_brackets_length = tax_constants["rent_data"]["brackets_length"]
+        self.pension_brackets_length = constants["pension_data"]["brackets_length"]
+        self.rent_brackets_length = constants["rent_data"]["brackets_length"]
 
-        self.pension_brackets = self.parse_brackets_from_json("pension_data", tax_constants)
-        self.rent_brackets = self.parse_brackets_from_json("rent_data", tax_constants)
+        self.pension_brackets = self.parse_brackets_from_json("pension_data", constants)
+        self.rent_brackets = self.parse_brackets_from_json("rent_data", constants)
 
         return self.convert_bracket_values_to_dict(self.pension_brackets), self.convert_bracket_values_to_dict(self.rent_brackets)
 
-    def find_coefficient(self, tax_brackets: dict):
-        pass
+    def get_stamp_duty(self, data_field: str, constants_object: dict):
+        stamp_duty: float = constants_object[data_field]["stamp_duty"]
+        if stamp_duty == 0:
+            return None
+        return stamp_duty
 
     @staticmethod
     def parse_brackets_from_json(data_field: str, constants_object: dict):
@@ -57,7 +64,7 @@ if __name__ == "__main__":
     c1 = FinancialConstants()
 
     def print_dict_conversion(for_=False):
-        var1, var2 = c1.get_tax_brackets()
+        var1, var2 = c1.get_tax_brackets(c1.constants)
         if for_ is False:
             print(c1.convert_bracket_values_to_dict(var1))
             print(c1.convert_bracket_values_to_dict(var2))
@@ -72,5 +79,11 @@ if __name__ == "__main__":
         print(f"Pension: {c1.pension_brackets_length}")
         print(f"Rent: {c1.rent_brackets_length}")
 
-    print_dict_conversion(for_=True)
-    print_brackets_length()
+
+    c1.init_constants()
+    print(c1.pension_stamp_duty)
+    print(c1.rent_stamp_duty)
+    print(c1.pension_brackets_length)
+    print(c1.pension_brackets)
+    print(c1.rent_brackets_length)
+    print(c1.rent_brackets)
