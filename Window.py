2b99241f9d9e339769311cd1c98da1c8303f044a
Sadmentour <sadmentores@gmail.com>
2025-08-11 04:33:40 +0300

    No message

--- Window.py
+++ Window.py
@@ -1,2 +1,38 @@
-import ttkbootstrap as ttk
-ttk.Window().mainloop()
+import ttkbootstrap as tbs
+
+class Window(tbs.Window):
+    def __init__(
+        self, title="ttkbootstrap",
+        themename="litera",
+        iconphoto='',
+        size=None,
+        position=None,
+        minsize=None,
+        maxsize=None,
+        resizable=None,
+        hdpi=True,
+        scaling=None,
+        transient=None,
+        overrideredirect=False,
+        alpha=1,
+        **kwargs
+    ):
+        super().__init__(
+            title,
+            themename,
+            iconphoto,
+            size,
+            position,
+            minsize,
+            maxsize,
+            resizable,
+            hdpi,
+            scaling,
+            transient,
+            overrideredirect,
+            alpha,
+            **kwargs
+        )
+
+    def create_widget_objects(self):
+        pass
