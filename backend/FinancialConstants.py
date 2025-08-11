aee0bb4a56efe446f644138036268161c54325a9
Sadmentour <sadmentores@gmail.com>
2025-08-11 13:54:55 +0300

    Major bux fixes and dictionary headache

--- backend/FinancialConstants.py
+++ backend/FinancialConstants.py
@@ -3,21 +3,27 @@ import json
 class FinancialConstants:
     def __init__(self):
         self.pension_brackets: dict = {}
+        self.pension_brackets_length: int = 0
         self.rent_brackets: dict = {}
+        self.rent_brackets_length: int = 0
         self.pension_brackets, self.rent_brackets = self.get_tax_brackets()
 
     def get_constants(self):
         pass
 
-    def get_tax_brackets(self, file: str = "financialConstants.json"):
+    def get_tax_brackets(self, file: str = "backend/financialConstants.json"):
         pension_brackets: dict = {}
         rent_brackets: dict = {}
         with open(file, "r") as constants:
             tax_constants: dict = json.load(constants)
+
+        self.pension_brackets_length = tax_constants["pension_data"]["brackets_length"]
+        self.rent_brackets_length = tax_constants["rent_data"]["brackets_length"]
+
         self.pension_brackets = self.parse_brackets_from_json("pension_data", tax_constants)
         self.rent_brackets = self.parse_brackets_from_json("rent_data", tax_constants)
 
-        return self.parse_bracket_values(self.pension_brackets), self.parse_bracket_values(self.pension_brackets)
+        return self.convert_bracket_values_to_dict(self.pension_brackets), self.convert_bracket_values_to_dict(self.rent_brackets)
 
     @staticmethod
     def parse_brackets_from_json(data_field: str, constants_object: dict):
@@ -35,7 +41,7 @@ class FinancialConstants:
         annual_income = self.monthly_to_yearly(monthly_income)
         final_result = ...
 
-    def parse_bracket_values(self, tax_brackets: dict):
+    def convert_bracket_values_to_dict(self, tax_brackets: dict):
         bracket_dict: dict = {}
         for bracket_index, bracket_content in tax_brackets.items():
 
@@ -50,7 +56,22 @@ class FinancialConstants:
 
 if __name__ == "__main__":
     c1 = FinancialConstants()
-    var1, var2 = c1.get_tax_brackets()
-    # for i in c1.parse_bracket_values(var1):
-    #     print(i)
-    print(c1.parse_bracket_values(var1))
+
+    def print_dict_conversion(for_=False):
+        var1, var2 = c1.get_tax_brackets()
+        if for_ is False:
+            print(c1.convert_bracket_values_to_dict(var1))
+            print(c1.convert_bracket_values_to_dict(var2))
+            return None
+        for i, j in var1.items():
+            print(f"{i}: {j}")
+        print("---------------------------")
+        for i, j in var2.items():
+            print(f"{i}: {j}")
+
+    def print_brackets_length():
+        print(f"Pension: {c1.pension_brackets_length}")
+        print(f"Rent: {c1.rent_brackets_length}")
+
+    print_dict_conversion(for_=True)
+    print_brackets_length()
