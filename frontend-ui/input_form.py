6430d67245fd73df46be5a0ae81d78e97516471f
Sadmentour <sadmentores@gmail.com>
2025-08-14 19:23:57 +0300

    Finished 'input_form.py' in frontend-ui

--- frontend-ui/input_form.py
+++ frontend-ui/input_form.py
@@ -20,30 +20,27 @@ class InputForm:
     def init_income_tax_widgets(self):
         self.containter_frame: FRM = tbs.Frame(self.master)
 
-        self.pension_labelframe: LBF = tbs.Labelframe(self.containter_frame, text="Pension")
-        self.rent_labelframe: LBF = tbs.Labelframe(self.containter_frame, text="Rent")
+        self.pension_labelframe: LBF = tbs.Labelframe(self.containter_frame, text="Monthly Pension")
+        self.rent_labelframe: LBF = tbs.Labelframe(self.containter_frame, text="Monthly Rent")
 
-        self.pension_label: LBL = tbs.Label(self.pension_labelframe, text=f"{self.income_prefix} Pension Income")
-        self.pension_entry: ENT = tbs.Entry(self.pension_labelframe)
+        self.pension_entry: ENT = tbs.Entry(self.pension_labelframe, width=15)
 
-        self.rent_label: LBL = tbs.Label(self.rent_labelframe, text=f"{self.income_prefix} Rent Income")
-        self.rent_entry: ENT = tbs.Entry(self.rent_labelframe)
+        self.rent_entry: ENT = tbs.Entry(self.rent_labelframe, width=15)
 
     def place_income_tax_widgets(self, grid_row: int, grid_column: int, grid_margin_xy: float, widget_margin_xy: float):
         self.containter_frame.grid(row=grid_row, column=grid_column, padx=grid_margin_xy, pady=grid_margin_xy)
 
-        self.pension_labelframe.grid(row=0, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
-        self.rent_labelframe.grid(row=0, column=1, padx=widget_margin_xy, pady=widget_margin_xy)
+        self.pension_labelframe.grid(row=0, column=0, padx=widget_margin_xy+1, pady=widget_margin_xy)
+        self.rent_labelframe.grid(row=0, column=1, padx=widget_margin_xy+1, pady=widget_margin_xy)
 
-        self.pension_label.grid(row=0, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
-        self.pension_entry.grid(row=1, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
-        self.rent_label.grid(row=0, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
-        self.rent_entry.grid(row=1, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
+        self.pension_entry.grid(row=0, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
+
+        self.rent_entry.grid(row=0, column=0, padx=widget_margin_xy, pady=widget_margin_xy)
 
 
 if __name__ == "__main__":
     print(f"input_form.py: {__name__}")
-    w = tbs.Window()
+    w = tbs.Window(themename="superhero")
     inp = InputForm(w)
-    inp.place_income_tax_widgets(0, 0, 10, 1)
+    inp.place_income_tax_widgets(0, 0, 2, 1)
     w.mainloop()
